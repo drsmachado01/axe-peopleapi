c80758c144a2263e6adfa968ce15c3da2f2db9da
drsmachado01 <dr.bsi@hotmail.com>
2021-10-03 01:41:39 -0300

    Adjusting map struct

--- src/main/java/br/com/axe/peopleapi/dto/response/PersonResponse.java
+++ src/main/java/br/com/axe/peopleapi/dto/response/PersonResponse.java
@@ -3,12 +3,14 @@ package br.com.axe.peopleapi.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class PersonResponse {
     private Long id;
--- src/main/java/br/com/axe/peopleapi/dto/response/PhoneResponse.java
+++ src/main/java/br/com/axe/peopleapi/dto/response/PhoneResponse.java
@@ -4,9 +4,11 @@ import br.com.axe.peopleapi.enums.PhoneType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class PhoneResponse {
     private Long id;
