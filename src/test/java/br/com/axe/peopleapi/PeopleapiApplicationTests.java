00f64051e2d18b3b931bb3c265f9c1285ccf2175
drsmachado01 <dr.bsi@hotmail.com>
2021-10-02 21:30:07 -0300

    Creating project

---
+++ src/test/java/br/com/axe/peopleapi/PeopleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axe.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
